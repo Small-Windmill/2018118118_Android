52f883bf9998daf990a3d9d8889411e86e1b8939
Small-Windmill <62464147+Small-Windmill@users.noreply.github.com>
2020-11-28 16:21:35 +0800

    作业10

---
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.servicetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startService = (Button)findViewById(R.id.start_service);
+        Button stopService = (Button)findViewById(R.id.stop_service);
+        Button startIntentService = (Button)findViewById(R.id.start_intent_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+        startIntentService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.start_service:
+                Intent startIntent = new Intent(this,MyService.class);
+                startService(startIntent);//启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this,MyService.class);
+                stopService(stopIntent);//启动服务
+                break;
+            case R.id.start_intent_service:
+                //打印主线程的id
+                Log.d("MainActivity","Thread id is "+Thread.currentThread().getId());
+                Intent intentService = new Intent(this,MyIntentService.class);
+                startService(intentService);
+                break;
+            default:
+                break;
+        }
+    }
+}
