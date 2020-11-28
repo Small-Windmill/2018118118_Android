3ede0e2f2ac47e89b3daa5f33c8a3276aef67cde
Small-Windmill <62464147+Small-Windmill@users.noreply.github.com>
2020-11-28 13:17:38 +0800

    作业8

---
+++ AndroidThreadTest/app/src/main/java/com/example/androidthreadtest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.androidthreadtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int UPDATE_TEXT =1;
+    private TextView text;
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case UPDATE_TEXT:
+//                   //进行UI操作
+                    text.setText("Nice to meet you");
+                    break;
+                 default:
+                     break;
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView)findViewById(R.id.text);
+        Button changeText = (Button)findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);//将Message对象发送出去
+                    }
+                }).start();
+                break;
+             default:
+                 break;
+        }
+    }
+}
